5472b51cd38226c90fdc98f09865f850bcafbe34
Cristian <criricyt@gmail.com>
2024-03-31 12:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ fisrt-task-cs/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome:");
+var name = Console.ReadLine();
+Console.WriteLine($"Olá, {name}! Seja muito bem-vindo!");
